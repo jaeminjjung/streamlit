dc3d1a9f0004c6d766da156347c7c82e6e585900
jaeminjjung <39325444+jaeminjjung@users.noreply.github.com>
2024-10-30 17:19:32 +0900

    Update index.py

--- index.py
+++ index.py
@@ -4,61 +4,28 @@ import pandas as pd
 import streamlit as st
 import plotly.figure_factory as ff
 import plotly.express as px
-import numpy as np
+import matplotlib.pyplot as plt
+import squarify
 """
 # Let's publish the result. :)
 """
 
-# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-# indices = np.linspace(0, 1, num_points)
-# theta = 2 * np.pi * num_turns * indices
-# radius = indices
-
-# x = radius * np.cos(theta)
-# y = radius * np.sin(theta)
-
-# df = pd.DataFrame({
-#     "x": x,
-#     "y": y,
-#     "idx": indices,
-#     "rand": np.random.randn(num_points),
-# })
-
-# st.altair_chart(alt.Chart(df, height=700, width=700)
-#     .mark_point(filled=True)
-#     .encode(
-#         x=alt.X("x", axis=None),
-#         y=alt.Y("y", axis=None),
-#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
-#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-#     ))
-
-
+# Load gapminder data and filter for year 2007
 df = px.data.gapminder().query("year == 2007")
-fig = px.treemap(df, path=[px.Constant("world"), 'continent', 'country'], values='pop',
-                  color='lifeExp', hover_data=['iso_alpha'],
-                  color_continuous_scale='RdBu',
-                  color_continuous_midpoint=np.average(df['lifeExp'], weights=df['pop']))
-fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
-fig.show()
 
+fig_treemap = px.treemap(
+    df,
+    path=[px.Constant("world"), "continent", "country"],
+    values="pop",
+    color="lifeExp",
+    hover_data=["iso_alpha"],
+    color_continuous_scale="RdBu",
+    color_continuous_midpoint=np.average(df["lifeExp"], weights=df["pop"]),
+    )
 
+# Update layout for better presentation
+fig_treemap.update_layout(margin=dict(t=50, l=25, r=25, b=25))
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
-
-# Group data together
-hist_data = [x1, x2, x3]
-
-group_labels = ['Group 1', 'Group 2', 'Group 3']
-
-# Create distplot with custom bin_size
-fig = ff.create_distplot(
-        hist_data, group_labels, bin_size=[.1, .25, .5])
-
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
+# Display the treemap using Streamlit
+st.plotly_chart(fig_treemap)
