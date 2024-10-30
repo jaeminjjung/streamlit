be402b4c18788fff2ff72f387751887a628a6f33
jaeminjjung <39325444+jaeminjjung@users.noreply.github.com>
2024-10-30 16:22:06 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,54 @@
+import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+import plotly.figure_factory as ff
+"""
+# Let's publish the result. :)
+"""
+
+# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+
+# indices = np.linspace(0, 1, num_points)
+# theta = 2 * np.pi * num_turns * indices
+# radius = indices
+
+# x = radius * np.cos(theta)
+# y = radius * np.sin(theta)
+
+# df = pd.DataFrame({
+#     "x": x,
+#     "y": y,
+#     "idx": indices,
+#     "rand": np.random.randn(num_points),
+# })
+
+# st.altair_chart(alt.Chart(df, height=700, width=700)
+#     .mark_point(filled=True)
+#     .encode(
+#         x=alt.X("x", axis=None),
+#         y=alt.Y("y", axis=None),
+#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
+#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+#     ))
+
+
+
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
