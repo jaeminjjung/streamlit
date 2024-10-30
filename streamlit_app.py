b45ec3b6e280b9033b4c2e017e6fc1b61b391c9e
jaeminjjung <39325444+jaeminjjung@users.noreply.github.com>
2024-10-30 16:40:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ import pandas as pd
 import streamlit as st
 import plotly.figure_factory as ff
 import plotly.express as px
+import matplotlib.pyplot as plt
+import squarify
+
+
 """
 # Welcome to Streamlit!
 
@@ -50,6 +54,19 @@ In the meantime, below is an example of what you can do with just a few lines of
 # fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
 # fig.show()
 
+
+volume = [350, 220, 170, 150, 50]
+labels = ['Liquid\n volume: 350k', 'Savoury\n volume: 220k',
+         'Sugar\n volume: 170k', 'Frozen\n volume: 150k',
+         'Non-food\n volume: 50k']
+color_list = ['#0f7216', '#b2790c', '#ffe9a3',
+             '#f9d4d4', '#d35158', '#ea3033']
+
+plt.rc('font', size=14)
+squarify.plot(sizes=volume, label=labels,
+             color=color_list, alpha=0.7)
+plt.axis('off')
+st.pyplot()
 
 # Add histogram data
 x1 = np.random.randn(200) - 2
